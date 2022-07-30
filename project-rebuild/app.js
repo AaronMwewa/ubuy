82535b3093c41d8bebfb91868d0e9af48a1c1c9a
AaronMwewa <97737768+AaronMwewa@users.noreply.github.com>
2022-07-30 18:18:48 +0200

    Add files via upload

---
+++ project-rebuild/app.js
@@ -0,0 +1,18 @@
+var menuIcon = document.querySelector(".menu-icon")
+var menuItems = document.querySelector('#menu-items')
+
+let winWidth = window.innerWidth
+
+if (window.width < '651px') {
+    console.log("happy")
+}
+
+
+
+
+
+
+
+
+
+///well done
